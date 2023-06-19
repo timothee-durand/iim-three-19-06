1cffc556600d3f2516317db62fac79533e59f256
Timothée DURAND <timothee.durand70@gmail.com>
2023-06-19 11:09:59 +0200

    fix camera and make some adjustments

--- src/script.js
+++ src/script.js
@@ -18,10 +18,8 @@ const options = {
 const scene = new Scene()
 scene.background = new Color(0x7057ff)
 
-const camera = new PerspectiveCamera(1000, options.width / options.height)
-camera.position.z = -4
-camera.position.y = -3
-camera.position.x = -1
+const camera = new PerspectiveCamera(45, options.width / options.height)
+camera.position.set(3, 7, -7)
 scene.add(camera)
 
 const canvas = document.querySelector('#renderer')
@@ -29,7 +27,7 @@ const renderer = new WebGLRenderer({canvas: canvas})
 
 const spotLight = new SpotLight(0xffffff, 2)
 scene.add(spotLight)
-spotLight.position.set(0, -2, -6)
+spotLight.position.set(0, 4, -6)
 
 
 const plane = new Mesh(
@@ -37,8 +35,8 @@ const plane = new Mesh(
     new MeshStandardMaterial({color: 0x005f00})
 )
 scene.add(plane)
-plane.position.set(-2, 2, 2)
-plane.rotation.x = Math.PI / 2
+plane.position.set(-2, -2, 2)
+plane.rotation.x = -Math.PI / 2
 
 
 const sphereGroup = new Group()
@@ -55,7 +53,7 @@ sphereGroup.add(redSphere)
 
 const metalTexture = await loadTexture('/textures/metal.jpg')
 const sphere3 = getSphere({texture: metalTexture})
-sphere3.position.set(0, 1, -1)
+sphere3.position.set(0, 0, -1)
 scene.add(sphere3)
 
 
@@ -76,7 +74,7 @@ animate()
 function getSphere({color, texture}) {
     return new Mesh(
         new SphereGeometry(0.5, 10, 10),
-        new MeshBasicMaterial({color: color, map: texture})
+        new MeshStandardMaterial({color: color, map: texture})
     )
 }
 
